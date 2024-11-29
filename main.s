a41d2a88ce1fe528c1a02b844ea8726b6c669fe5
jy3122 <jy3122@skbl-418-008.ph.ic.ac.uk>
2024-11-29 11:51:41 +0000

    3rd session

--- main.s
+++ main.s
@@ -1,42 +1,56 @@
 #include <xc.inc>
 ; Declare external subroutines
 extrn    Setup_Timer, Button_Pressed,Start_Timer,Button_Released,Process_Timer,TIMER0_ISR
-extrn    Elapsed_Time_H, Elapsed_Time_M,Elapsed_Time_L
+extrn    Elapsed_Time_L,Elapsed_Time_M,Elapsed_Time_H  
 extrn    LCD_Setup, LCD_Write_Message,LCD_Write_Hex, LCD_Send_Byte_D
 ; Data section for storing variables in access RAM
-psect    udata_acs   
-;high: ds  1   ; Variable to store the time result
-;mid:  ds  1
-;low:  ds  1
+    
+    
+psect    udata   
 delay_count: ds 1      ; Variable for delay routine counter
-; Code section for main program
+
+    
+    
 psect	code, abs	
-rst: 	org 0x0000
-	goto	setup
+
+rst: org 0x0000
+     goto setup
+     
+     
+interrupt: org 0x0008          ; High-priority interrupt vector
+           goto TIMER0_ISR     ; Jump to Timer0 interrupt service routine
+
  
 ; Main program entry point
 setup:
-    ;clrf high,A
-    ;clrf mid,A
-    ;clrf low,A
-
-    call Setup_Timer     ; Initialize the keypad
+    call Setup_Timer     ; Initialize the timer
     call LCD_Setup        ; Initialize the LCD display
     goto Main_Loop_Start
     
 Main_Loop_Start:
-    call Process_Timer      ; Identify which column is pressed
-    movlw 0x80
-    call LCD_Send_Byte_D
-    movf Elapsed_Time_H,A
-    call LCD_Write_Hex   ; Send the detected key to the LCD display
-    movf Elapsed_Time_M,A
+    call Button_Pressed
+    call Start_Timer
+    call Button_Released
+    ;call Process_Timer      ; Identify which column is pressed
+    ;movlw 0x80
+    ;call LCD_Send_Byte_D
+    
+    
+    movf Elapsed_Time_H,W,A
+   
+    ; movf PORTD, W, A
+
+    ;call LCD_Write_Hex   ; Send the detected key to the LCD display
+    
+    ;movf Elapsed_Time_M,A
+    ;call LCD_Write_Hex 
+    
+    ;movf Elapsed_Time_L,W,A
+    
     call LCD_Write_Hex 
-    movf Elapsed_Time_L,A
-    call LCD_Write_Hex 
-    ;movlw 0xFF            ; Load 0xFF into W for delay counter initialization
-    ;movwf delay_count, A  ; Initialize delay counter
-    ;call delay            ; Call delay subroutine
+    movlw 0xFF            ; Load 0xFF into W for delay counter initialization
+    movwf delay_count, A  ; Initialize delay counter
+    call delay            ; Call delay subroutine
     goto Main_Loop_Start  ; Repeat the main loop
     
 ; Delay subroutine
